8113f71447a01a6b1e2034cf963a26adaac3d252
syamIT <syamdonkada255@gmail.com>
2024-10-18 00:10:10 +0530

    Created And Deployed

---
+++ script.js
@@ -0,0 +1,110 @@
+let para = document.getElementsByTagName("p")[3];
+console.log(para);
+para.innerText = "This is a Para Tag...."
+
+// let o1 = document.getElementById("1")
+// function imgCall(){
+//     o1.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o1.style.opacity = 0.1    
+// }
+
+// let o2 = document.getElementById("2")
+// function imgCall1(){
+//     o2.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o2.style.opacity = 0.2
+// }
+
+// let o3 = document.getElementById("3")
+// function imgCall2(){
+//     o3.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o3.style.opacity = 0.3
+// }
+
+// let o4 = document.getElementById("4")
+// function imgCall3(){
+//     o4.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o4.style.opacity = 0.4
+// }
+
+// let o5 = document.getElementById("5")
+// function imgCall4(){
+//     o5.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o5.style.opacity = 0.5
+// }
+
+// let o6 = document.getElementById("6")
+// function imgCall5(){
+//     o6.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o6.style.opacity = 0.6
+// }
+
+// let o7 = document.getElementById("7")
+// function imgCall6(){
+//     o7.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o7.style.opacity = 0.7
+// }
+
+// let o8 = document.getElementById("8")
+// function imgCall7(){
+//     o8.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o8.style.opacity = 0.8
+// }
+
+// let o9 = document.getElementById("9")
+// function imgCall8(){
+//     o9.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o9.style.opacity = 0.9
+// }
+// let o10 = document.getElementById("10")
+// function imgCall9(){
+//     o10.innerHTML = "<img src='Events in JS.png' alt= />"
+//     o10.style.opacity = 1.0
+// }
+
+
+let div = document.getElementsByTagName("div")[1]
+console.log(div);
+function imgCall(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0
+}
+function imgCall1(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.1
+}
+function imgCall2(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.2
+}
+function imgCall3(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.3
+}
+function imgCall4(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.4
+}
+function imgCall5(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.5
+}
+function imgCall6(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.6
+}
+function imgCall7(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.7
+}
+function imgCall8(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.8
+}
+function imgCall9(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 0.9
+}
+function imgCall10(){
+    div.innerHTML = "<img  src='Events in JS.png' alt= />"
+    div.style.opacity = 1
+}
